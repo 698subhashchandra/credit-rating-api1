c0ca20d80651eeb043c52733157a0e7d67238cc8
vikash <subhashchandrayadav698@gmail.com>
2025-01-29 17:39:19 +0530

    add terraform folder

---
+++ terrform/variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "instance_ami" {
+  description = "AMI ID for EC2 instance"
+  type        = string
+  default     = "ami-0c7217cdde317cfec"  # Ubuntu 22.04 LTS in us-east-1
+}
