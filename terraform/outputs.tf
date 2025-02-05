353227f766563b26e02765b4cf8281d209b41d46
698subhashchandra <135433048+698subhashchandra@users.noreply.github.com>
2025-02-05 17:11:19 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,3 @@
 output "instance_public_ip" {
-  value = aws_instance.my_ec2.public_ip
-}
-
-output "instance_id" {
-  value = aws_instance.my_ec2.id
+  value = aws_instance.ubuntu_instance.public_ip
 }
