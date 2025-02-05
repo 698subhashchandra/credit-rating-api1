fefb420c624e4f6f04e9726ead3a2337498565d1
698subhashchandra <135433048+698subhashchandra@users.noreply.github.com>
2025-02-05 17:12:02 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,22 +1,7 @@
-variable "aws_region" {
-  description = "AWS region to deploy resources"
-  default     = "us-east-1"
+variable "region" {
+  default = "us-east-1"
 }
 
 variable "instance_type" {
-  description = "EC2 instance type"
-  default     = "t2.micro"
-}
-
-variable "key_name" {
-  description = "SSH key pair name"
-  default     = "my-key-pair"
-}
-
-variable "vpc_cidr" {
-  default = "10.0.0.0/16"
-}
-
-variable "subnet_cidr" {
-  default = "10.0.1.0/24"
+  default = "t2.micro"
 }
